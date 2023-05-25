a685f5fd2691b7c8350f92d59512ef63b19c6391
Bhaskar Mahor <bhaskar.mahor.07@gmail.com>
2023-05-25 06:14:10 +0530

    bigo

---
+++ TimeAndSpace/BigO.py
@@ -0,0 +1,16 @@
+'''
+Big O Notation
+Big O notation is a mathematical notation that describes the miniting behavior of a function
+when the argument tends towards a particular value of inifinity.
+
+Different types of time complexity
+1. Constant time - O(1)
+2. Linear time - O(n)
+3. Logarithmic timem - O(log n)
+4. Quadratic time - O(n^2)
+5. Cubic time - O(n^3)
+'''
+
+
+
+
